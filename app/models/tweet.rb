c147f725888b171d05a5ebdd41b13b4e04b2f8ce
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-09-04 00:05:04 +0900

    コメント投稿時においても、投稿削除可能に修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :text, presence: true, unless: :was_attached?, length: { maximum: 140 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root to: "tweets#index"
   resources :tweets do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   resources :users
 end
