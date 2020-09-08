ece8af1afa76f37fe35817fa00f58dcc25a6b0ee
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-09-08 22:14:35 +0900

    タスク登録機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,21 @@ class TasksController < ApplicationController
     @tasks = Task.includes(:user).order("updated_at DESC")
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      redirect_to tasks_path
+    else
+      @tasks = Task.includes(:user).order("updated_at DESC")
+      render :index
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :tweets
   has_many :comments
+  has_many :tasks
 
   validates :name, presence: true
 end
