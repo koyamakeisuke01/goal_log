3100b52b1500234dab899fe276d8fb27c28532e4
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-09-15 10:44:48 +0900

    かんたんログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.guest
+    find_or_create_by(email: 'goal@goal.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
   has_many :tweets
   has_many :comments
   has_many :tasks
