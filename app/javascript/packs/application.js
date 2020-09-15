2ddd7b5e9c33b0a3e15b729298a24dd07a0b69f2
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-09-16 00:57:41 +0900

    jqueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("./jquery_jscroll_min")
 require("./tweet_image_preview")
 require("./profile_avatar_preview")
 
