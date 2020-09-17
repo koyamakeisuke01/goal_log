00c51b5f052f99db3d7a1898f696ef4e91d78cdf
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-09-17 11:51:55 +0900

    ユーザー新規登録ができないときの結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -29,4 +29,25 @@ RSpec.describe "ユーザー新規登録", type: :system do
       expect(page).to have_no_content('ログイン')
     end
   end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user_name', with: ""
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      fill_in 'user_password_confirmation', with: ""
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
 end
